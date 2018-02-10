1bc05903f3b77cce1429ff1ae0e75b3ce4872115
Trevor <trevcj@gmail.com>
2018-02-10 09:49:59 -0700

    serverside

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [{
+    name: "Darwood Martin",    
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]    
+  }, {
+    name: "Joel Joely",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]    
+  }, {
+    name: "Jan Jorg",    
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]   
+  },{
+    name: "Nick Craft",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Chris James",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Anna Feliciano",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Nick Feliciano",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
